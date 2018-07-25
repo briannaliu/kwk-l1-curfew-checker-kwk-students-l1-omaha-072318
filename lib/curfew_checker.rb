f7ac0504dfc59cecd7dce5caaa987458dba744ca
briannaliu <briannaliu3@gmail.com>
2018-07-25 21:15:00 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -38,7 +38,7 @@ def deluxe_curfew_checker(time)
 end
 
 def platinum_curfew_checker(current_time, curfew_time)
-  if current_time == 11
+  if current_time == 11 && curfew_time == 11
     "You're in trouble!"
   elsif current_time > 11
     "You're in trouble! Better get back to Hogwarts quick!"
