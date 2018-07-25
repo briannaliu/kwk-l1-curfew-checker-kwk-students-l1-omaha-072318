20b3608540fed58959535fb7cf9040593e94b1b9
briannaliu <briannaliu3@gmail.com>
2018-07-25 21:00:58 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -5,11 +5,15 @@ def simple_curfew_checker(time)
 end
 
 def curfew_checker(time)
-  # code goes here
+  if time > 11
+    puts "You're in trouble! Better get home quick!"
+  end
 end
 
 def complex_curfew_checker(time)
-  # code goes here
+  if time < 11
+    puts "Keep having fun!"
+  end
 end
 
 def deluxe_curfew_checker(time)
